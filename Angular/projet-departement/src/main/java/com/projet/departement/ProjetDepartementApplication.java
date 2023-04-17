3d0f4407ae7e1508aacdaad33671c52c40ae2839
leonel.dongmo <maken.dongmo@boostcompagny.com>
2023-04-17 17:26:24 +0100

    dépôt git

---
+++ Angular/projet-departement/src/main/java/com/projet/departement/ProjetDepartementApplication.java
@@ -0,0 +1,13 @@
+package com.projet.departement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDepartementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDepartementApplication.class, args);
+	}
+
+}
